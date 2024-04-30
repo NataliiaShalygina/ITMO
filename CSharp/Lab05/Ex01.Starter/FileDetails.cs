909eff22fca215b595c8a4b21f9c2c51037a0a34
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2024-04-30 19:05:45 +0500

    Update FileDetails.cs

--- CSharp/Lab05/Ex01.Starter/FileDetails.cs
+++ CSharp/Lab05/Ex01.Starter/FileDetails.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Starter
 {
@@ -10,13 +7,53 @@ namespace Starter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(args.Length);
-            foreach (string arg in args)
+            string fileName = args[0];
+            FileStream stream = new FileStream(fileName, FileMode.Open);
+            StreamReader reader = new StreamReader(stream);
+            
+            int size = (int)stream.Length;
+            char[] contents = new char[size];
+            for (int i = 0; i < size; i++)
             {
-                Console.WriteLine(arg);
+                contents[i] = (char)reader.Read();
             }
-            Console.ReadLine();
-        }
 
+            /*
+            foreach (char ch in contents)
+            {
+                Console.Write(ch);
+            }
+            */
+
+            reader.Close();
+            stream.Close();
+            Summarize(contents);
+        }
+        static void Summarize(char[] contents)
+        {
+            int vowels = 0, consonants = 0, lines = 0;
+            foreach (char current in contents)
+            {
+                if (Char.IsLetter(current))
+                {
+                    if ("AEIOUaeiou".IndexOf(current) != -1)
+                    {
+                        vowels++;
+                    }
+                    else
+                    {
+                        consonants++;
+                    }
+                }
+                else if (current == '\n')
+                {
+                    lines++;
+                }
+            }
+            Console.WriteLine("Total no of characters: {0}", contents.Length);
+            Console.WriteLine("Total no of vowels : {0}", vowels);
+            Console.WriteLine("Total no of consonants: {0}", consonants);
+            Console.WriteLine("Total no of lines : {0}", lines);
+        }
     }
 }
