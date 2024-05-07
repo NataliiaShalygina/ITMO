395eaa774d61ef1c93c14bfc6e686f58a3bbe284
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2024-05-07 22:47:20 +0500

    Add files via upload

---
+++ CSharp/Lab09/Starter/Line_end_token.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Starter
+{
+    internal sealed class LineEnd
+    {
+        internal sealed class Token : ILineEndToken
+        {
+            internal Token(int number)
+            {
+                this.number = number;
+            }
+
+            public static Token operator ++(Token t)
+            {
+                t.number++;
+                return t;
+            }
+
+            int ILineEndToken.Number()
+            {
+                return number;
+            }
+
+            private int number;
+        }
+    }
+
+}
---
+++ CSharp/Lab09/Starter/Main.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Starter
+{
+    public class Application
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                InnerMain(args);
+            }
+            catch (System.Exception caught)
+            {
+                Console.WriteLine(caught);
+            }
+        }
+
+        public static void InnerMain(string[] args)
+        {
+            SourceFile source = new SourceFile(args[0]);
+
+            HTML_token_visitor visitor = new HTML_token_visitor();
+            source.Accept(visitor);
+        }
+    }
+}
