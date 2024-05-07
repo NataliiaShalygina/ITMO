75346592e77de71682bc7914ad72add9a95ead79
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2024-05-07 22:38:22 +0500

    Add files via upload

---
+++ CSharp/Lab12/Exercise01/BankTransaction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+
+namespace Exercise01
+{
+    namespace Banking
+    {
+        public class BankTransaction
+        {
+            private readonly decimal amount;
+            private readonly DateTime when;
+
+            public BankTransaction(decimal tranAmount)
+            {
+                amount = tranAmount;
+                when = DateTime.Now;
+            }
+
+            public decimal Amount()
+            {
+                return amount;
+            }
+
+            public DateTime When()
+            {
+                return when;
+            }
+        }
+    }
+}
