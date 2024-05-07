3efc586b34e6c3da2a4a33fb81a531df6f4c7af8
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2024-05-07 22:40:31 +0500

    Add files via upload

---
+++ CSharp/Lab12/Exercise02/AuditEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercise02
+{
+    namespace Banking
+    {
+        public class AuditEventArgs : System.EventArgs
+        {
+            private readonly BankTransaction transData = null;
+
+            public AuditEventArgs(BankTransaction transaction)
+            {
+            this.transData = transaction;
+            }
+            public BankTransaction getTransaction()
+            {
+            return this.transData;
+            }
+        }
+    }
+}
