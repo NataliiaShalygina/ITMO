c661c42b5f7bea84ae61088fc6fe26b1db81b70f
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-04-11 21:03:05 +0500

    Update menu.js

--- WebApp_HTML_CSS_JS/Website/js/menu.js
+++ WebApp_HTML_CSS_JS/Website/js/menu.js
@@ -1,8 +1,12 @@
 function myFunc() {
 var dropdownContent =document.getElementById("myDropdown");
+if (dropdownContent== "") {
+           alert("Ошибка, пустое значение") 
+        	     } else {
 if (dropdownContent.style.display === "block") {
 				dropdownContent.style.display = "none";
 						} else {
 				dropdownContent.style.display = "block";
 							}
 		}
+}
