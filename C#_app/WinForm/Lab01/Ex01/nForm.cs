952f172063ce6c616caf6d754e7522e835854da3
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-04-21 03:14:02 +0500

    Create nForm.cs

---
+++ C#_app/WinForm/Lab01/Ex01/nForm.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex01
+{
+    public partial class nForm : Form
+    {
+        public nForm()
+        {
+            InitializeComponent();
+        }
+
+        private bool nclose = false;
+        public new void Close()
+        {
+           nclose = true; 
+           base.Close();
+        }
+        private void nForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (checkBoxClose.Checked) return;
+            if (nclose) return;
+            e.Cancel = true;
+            Hide();
+        }
+
+  
+
+        private void nForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
