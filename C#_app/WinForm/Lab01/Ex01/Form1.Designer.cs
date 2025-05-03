0ae962b2fd63bfce8b4d574aec5c0d311c919ddc
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-03 16:32:28 +0500

    Update Form1.Designer.cs

--- C#_app/WinForm/Lab01/Ex01/Form1.Designer.cs
+++ C#_app/WinForm/Lab01/Ex01/Form1.Designer.cs
@@ -2,15 +2,9 @@ namespace Ex01
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private System.ComponentModel.IContainer components = null;
+        
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +16,7 @@ namespace Ex01
 
         #region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
+      
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
