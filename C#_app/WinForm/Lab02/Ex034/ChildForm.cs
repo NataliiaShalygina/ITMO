a733436a4f34ccbfa8614959658a5fadebd89be2
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-03 17:41:08 +0500

    Create ChildForm.cs

---
+++ C#_app/WinForm/Lab02/Ex034/ChildForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace MdiApplication
+{
+    public partial class ChildForm : Form
+    {
+        public ChildForm()
+        {
+            InitializeComponent();
+        }
+
+        private void formatToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToggleMenuItem_Click(object sender, EventArgs e)
+        {
+            if (ToggleMenuItem.Checked)
+            {
+                ToggleMenuItem.Checked = false;
+                ChildTextBox.ForeColor = System.Drawing.Color.Black;
+            }
+            else
+            {
+                ToggleMenuItem.Checked = true;
+                ChildTextBox.ForeColor = System.Drawing.Color.Blue;
+            }
+        }
+    }
+}
