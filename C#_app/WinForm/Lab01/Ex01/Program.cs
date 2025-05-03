12bf7ef295372074884c141b6e1c9a7ea24678b4
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-03 16:33:52 +0500

    Update Program.cs

--- C#_app/WinForm/Lab01/Ex01/Program.cs
+++ C#_app/WinForm/Lab01/Ex01/Program.cs
@@ -8,9 +8,7 @@ namespace Ex01
 {
     internal static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
+
         [STAThread]
         static void Main()
         {
