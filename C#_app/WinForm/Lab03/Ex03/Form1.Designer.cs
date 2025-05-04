a530de03205d0bf0fb92c9c0351bd93ad64ea169
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-04 19:25:16 +0500

    Create Form1.Designer.cs

---
+++ C#_app/WinForm/Lab03/Ex03/Form1.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace WinButNum
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+       
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        
+        private void InitializeComponent()
+        {
+            this.clickButton1 = new WinButNum.ClickButton();
+            this.SuspendLayout();
+            // 
+            // clickButton1
+            // 
+            this.clickButton1.Location = new System.Drawing.Point(180, 110);
+            this.clickButton1.Name = "clickButton1";
+            this.clickButton1.Size = new System.Drawing.Size(156, 23);
+            this.clickButton1.TabIndex = 0;
+            this.clickButton1.Text = "clickButton1";
+            this.clickButton1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.clickButton1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ClickButton clickButton1;
+    }
+}
