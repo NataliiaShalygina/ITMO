053296ddf218b0fe64791e9b89ca4d75dc2ddff9
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-04 11:32:44 +0500

    Create Form1.cs

---
+++ C#_app/WinForm/Lab03/Ex02/Form1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinTimer2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
