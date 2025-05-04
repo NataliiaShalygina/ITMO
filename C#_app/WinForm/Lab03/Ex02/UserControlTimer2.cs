8e4659e1324250bdf6294b5178c0c0962b20b74f
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-04 19:19:20 +0500

    Create UserControlTimer2.cs

---
+++ C#_app/WinForm/Lab03/Ex02/UserControlTimer2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinTimer2
+{
+    public partial class UserControlTimer2 : UserControl
+    {
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+            Graphics g = pe.Graphics;
+            g.FillRectangle(Brushes.Blue, 0, 0, this.Width, this.Height);
+            pe.Graphics.DrawString(DateTime.Now.ToLongTimeString(), this.Font, new
+            SolidBrush(this.ForeColor), 0, 0);
+        }
+        public UserControlTimer2()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+    }
+}
