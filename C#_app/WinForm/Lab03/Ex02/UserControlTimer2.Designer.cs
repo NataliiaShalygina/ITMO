89b92684313ef0672b9dd6c97bd2585aac4c2e8d
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-04 19:21:02 +0500

    Create UserControlTimer2.Designer.cs

---
+++ C#_app/WinForm/Lab03/Ex02/UserControlTimer2.Designer.cs
@@ -0,0 +1,46 @@
+namespace WinTimer2
+{
+    partial class UserControlTimer2
+    {
+
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // UserControlTimer2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Name = "UserControlTimer2";
+            this.Size = new System.Drawing.Size(286, 271);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+    }
+}
