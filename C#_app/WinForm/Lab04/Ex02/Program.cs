2abfe7632f30f73e710cf70e5947ac7b3c520524
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-04 19:35:05 +0500

    Create Program.cs

---
+++ C#_app/WinForm/Lab04/Ex02/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EditPerson
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public int indexList;
+
+        public override string ToString()
+        {
+            return LastName + " " + FirstName + "\nВозраст: " + Age + "\n" + indexList;
+        }
+    }
+
+}
