e7685a14ade3bfb91ef70f58685bbef10045383c
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-06 13:46:20 +0500

    Create Form2.cs

---
+++ C#_app/WinForm/Lab04/Ex05/Form2.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ex05
+{
+    public partial class Form2 : Form
+    {
+        public bool radians = true;
+
+        public decimal min
+        {
+            get { return numericUpDown1.Value; }
+            set { numericUpDown1.Value = value; }
+        }
+
+        public decimal max 
+        {
+            get { return numericUpDown2.Value; }
+            set { numericUpDown2.Value = value; }
+        }
+
+        
+        public Form2()
+        {
+            InitializeComponent();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            radians = true;
+            numericUpDown1.DecimalPlaces = 2;
+            numericUpDown2.DecimalPlaces = 2;
+            numericUpDown1.Increment = (decimal)0.01;
+            numericUpDown2.Increment = (decimal)0.01;
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            radians = false;
+            numericUpDown1.DecimalPlaces = 0;
+            numericUpDown2.DecimalPlaces = 0;
+            numericUpDown1.Increment = 1;
+            numericUpDown2.Increment = 1;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+               
+        }
+    }
+}
