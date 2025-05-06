230e3166ddee3aa139e3e56fbd9e79ec1b7d8296
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-06 17:24:57 +0500

    Update Form1.Designer.cs

--- C#_app/WinForm/Lab08/EditPersonXML/Form1.Designer.cs
+++ C#_app/WinForm/Lab08/EditPersonXML/Form1.Designer.cs
@@ -2,15 +2,9 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Требуется переменная конструктора.
-        /// </summary>
+      
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Освободить все используемые ресурсы.
-        /// </summary>
-        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -22,10 +16,6 @@
 
         #region Код, автоматически созданный конструктором форм Windows
 
-        /// <summary>
-        /// Обязательный метод для поддержки конструктора - не изменяйте
-        /// содержимое данного метода при помощи редактора кода.
-        /// </summary>
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
