43467fc547d9692af92d128397dde911fbf35190
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-06 22:52:01 +0500

    Create CustomCommands.cs

---
+++ C#_app/WPF/Lab01/CustomCommands.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WpfHello
+{
+    class CustomCommands
+    {
+        public static RoutedUICommand Launch { get; }
+        static CustomCommands()
+        {
+            InputGestureCollection myInputGesture = new InputGestureCollection();
+            myInputGesture.Add(new KeyGesture(Key.L, ModifierKeys.Control));
+            Launch = new RoutedUICommand("Запуск", "Launch", typeof(CustomCommands), myInputGesture);
+        }
+    }
+}
