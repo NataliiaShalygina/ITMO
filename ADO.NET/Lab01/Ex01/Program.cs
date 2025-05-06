5dcc424e3f1cf25f55f5de2e301fba2e23103635
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-06 23:01:23 +0500

    Create Program.cs

---
+++ ADO.NET/Lab01/Ex01/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Ex01
+{
+    internal static class Program
+    {
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
