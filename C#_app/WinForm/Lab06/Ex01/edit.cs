b6e767fe75ee5793ad85adf7e06db346ae8e84ba
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-06 14:18:55 +0500

    Create edit.cs

---
+++ C#_app/WinForm/Lab06/Ex01/edit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinGrafSinLine
+{
+    public partial class edit : Form
+    {
+        public edit()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 frm1 = this.Owner as Form1;
+            try
+            {
+                frm1.a = double.Parse(textBox1.Text);
+                frm1.b = double.Parse(textBox2.Text);
+                frm1.DataA = textBox1.Text;
+                frm1.DataB = textBox2.Text;
+            }
+            catch (Exception er)
+            {
+                MessageBox.Show("При выполнении ввода исходных данных возникла ошибка: \n" + er.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            this.Close();
+            frm1.Refresh();
+        }
+    }
+}
