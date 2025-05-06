c97390a2fb50a694af8bc76675bcdae928e4ceac
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-06 14:15:03 +0500

    Create Program.cs

---
+++ C#_app/WinForm/Lab06/Ex01/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace WinGrafSinLine
+{
+    internal static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+    class Operation
+    {
+        public static double SummSin(int x, double a, double b)
+        {
+            double res;
+            return res = Math.Sin(x * a) + Math.Sin(x * b);
+        }
+    }
+}
