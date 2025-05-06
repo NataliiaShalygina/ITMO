d541143d75662716fa5f0473fa877d65e3d736c4
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-06 17:22:29 +0500

    Create EditPersonForm.cs

---
+++ C#_app/WinForm/Lab08/EditPersonXML/EditPersonForm.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EditPersonXML
+{
+    public partial class EditPersonForm : Form
+    {
+       
+        Person p = new Person();
+        public EditPersonForm()
+        {
+            InitializeComponent();
+
+
+        }
+
+        public string FirstName
+        {
+            get { return firstNameTextBox.Text; }
+            set { firstNameTextBox.Text = value; }
+        }
+
+        public string LastName
+        {
+            get { return lastNameTextBox.Text; }
+            set { lastNameTextBox.Text = value; }
+        }
+
+        public int Age
+        {
+            get { return (int)ageNumericUpDown.Value; }
+            set { ageNumericUpDown.Value = value; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            p.FirstName = firstNameTextBox.Text;
+            p.LastName = lastNameTextBox.Text;
+            p.Age = Convert.ToInt32(ageNumericUpDown.Value);
+            MessageBox.Show("Сотрудник: " + p.ToString());
+            Form1.pers.Add(p);
+            
+           
+        }
+
+        private void EditPersonForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
