caf68af27c026cac01d11aab1eb6b2e7d7e9e038
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-06 23:08:48 +0500

    Create MyWindow.xaml.cs

---
+++ C#_app/WPF/Lab01/MyWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace WpfHello
+{
+    public partial class MyWindow : Window
+    {
+        public MyWindow()
+        {
+            InitializeComponent();
+        }
+        private bool _close;
+        public new void Close()
+        {
+            _close = true;
+            base.Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (_close) return;
+            e.Cancel = true;
+            Hide();
+        }
+        MainWindow wnd1 = null;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            wnd1 = Owner as MainWindow;
+            if (wnd1 != null)
+            {
+                wnd1.textBlock.Text = textBox.Text;
+            }
+            Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            wnd1.myWin = null;
+        } 
+    }
+}
