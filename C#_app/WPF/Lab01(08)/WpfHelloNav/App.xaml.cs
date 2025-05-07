ae857c6d313c62580dee0a859391a46346ea2d01
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-07 16:58:33 +0500

    Create App.xaml.cs

---
+++ C#_app/WPF/Lab01(08)/WpfHelloNav/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace _03.WpfHelloNav__P1_E3_
+{
+    public partial class App : Application
+    {
+    }
+}
