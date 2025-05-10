3dc7c9eee2f0e7aace98824258d07fde555d79ef
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-10 18:36:40 +0500

    Create MainWindow.xaml.cs

---
+++ C#_app/WPF/Lab10/Ex02/MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WpfUserControl
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
