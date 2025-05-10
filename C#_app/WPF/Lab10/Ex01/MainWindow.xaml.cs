eea8f286f711d4bdbf90303e4e917772962cc54f
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-10 17:31:14 +0500

    Create MainWindow.xaml.cs

---
+++ C#_app/WPF/Lab10/Ex01/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Shapes;
+
+namespace WpfUserControl
+{
+    public partial class MainWindow : Window
+    {
+        List<string> PhoneNumbers = new List<String>();
+        System.Windows.Forms.SaveFileDialog aDialog;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.MaskedTextBox aBox;
+            aBox = (System.Windows.Forms.MaskedTextBox)windowsFormsHost1.Child;
+            PhoneNumbers.Add(aBox.Text);
+            aBox.Clear();
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            aDialog = new System.Windows.Forms.SaveFileDialog();
+            aDialog.Filter = "Text Files | *.txt";
+            aDialog.ShowDialog();
+            System.IO.StreamWriter myWriter = new
+            System.IO.StreamWriter(aDialog.FileName, true);
+            foreach (string s in PhoneNumbers)
+                myWriter.WriteLine(s);
+            myWriter.Close();
+        }
+    }
+}
