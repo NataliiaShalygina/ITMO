4e0b04ecf3f8193032ff9b0afce43e2755988fa7
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-12 13:25:10 +0500

    Create Department.cs

---
+++ ADO.NET/Lab09/02.CourseManager/Department.cs
@@ -0,0 +1,23 @@
+namespace CourseManager
+{
+    using System;
+    using System.Collections.Generic;
+    
+    public partial class Department
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public Department()
+        {
+            this.Courses = new HashSet<Course>();
+        }
+    
+        public int DepartmentID { get; set; }
+        public string Name { get; set; }
+        public decimal Budget { get; set; }
+        public System.DateTime StartDate { get; set; }
+        public Nullable<int> Administrator { get; set; }
+    
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Course> Courses { get; set; }
+    }
+}
