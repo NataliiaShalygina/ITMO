ca22bf8a6b90ee8e4abd1ecbd7ed05b7ecbaf7f4
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-28 17:13:35 +0500

    Create SampleContext.cs

---
+++ ASP/Zachet/SampleContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+
+namespace students
+{
+    public class SampleContext: DbContext
+    {
+        public SampleContext() : base("stdDB")
+        { }
+        public DbSet<Student> _Students { get; set; }
+
+    }
+}
