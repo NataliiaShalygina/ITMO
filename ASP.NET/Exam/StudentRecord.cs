a73a3b8b92db0a002de38972a5370dcafd7ba368
NataliiaShalygina <116905860+NataliiaShalygina@users.noreply.github.com>
2025-05-29 13:38:50 +0500

    Create StudentRecord.cs

---
+++ ASP.NET/Exam/StudentRecord.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Exam
+{
+    public class StudentRecord
+    {
+      public int StudentRecordId { get; set; }
+      public string FirstName { get; set; }
+      public string LastName { get; set; }
+      public string Group { get; set; }
+
+      public string Algebra { get; set; }
+      public string Geometry { get; set; }
+      public string MatchAnalysis { get; set; }
+      public string ProbTheory { get; set; }
+      public string Phisics { get; set; }
+
+      public int SumSub { get; set; }
+      public StudentRecord(string firstName, string lastName, string group, string algebra, string geometry, string mathAnalysis, string probTheory, string phisics)
+            {
+               FirstName = firstName;
+               LastName = lastName;
+               Group = group;
+               Algebra = algebra;
+               Geometry = geometry;
+               MatchAnalysis = mathAnalysis;
+               ProbTheory = probTheory;
+               Phisics = phisics;
+               SumSub = Convert.ToInt32(algebra) + Convert.ToInt32(geometry) + Convert.ToInt32(mathAnalysis) + Convert.ToInt32(probTheory) + Convert.ToInt32(phisics);
+            }
+        }
+    }
